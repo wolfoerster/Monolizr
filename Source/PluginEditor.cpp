1323373d18dd8593adc2f07f48e25bad552cedea
Wolfgang Förster <wolfoerster@gmx.de>
2026-05-10 00:51:13 +0200

    Second commit

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,49 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+MonolizrAudioProcessorEditor::MonolizrAudioProcessorEditor (MonolizrAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (200, 200);
+
+    midiVolume.setSliderStyle(juce::Slider::LinearBarVertical);
+    midiVolume.setRange(0.0, 100.0, 1.0);
+    midiVolume.setTextBoxStyle(juce::Slider::NoTextBox, false, 90, 0);
+    midiVolume.setPopupDisplayEnabled(true, false, this);
+    midiVolume.setTextValueSuffix(" Volume");
+    midiVolume.setValue(1.0);
+
+    addAndMakeVisible(&midiVolume);
+}
+
+MonolizrAudioProcessorEditor::~MonolizrAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void MonolizrAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // fill the whole window white
+    g.fillAll(juce::Colours::white);
+    // set the current drawing colour to black
+    g.setColour(juce::Colours::black);
+    // set the font size and draw text to the screen
+    g.setFont(15.0f);
+    g.drawFittedText("Midi Volume", 0, 0, getWidth(), 30, juce::Justification::centred, 1);
+}
+
+void MonolizrAudioProcessorEditor::resized()
+{
+    midiVolume.setBounds(40, 30, 20, getHeight() - 60);
+}
---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class MonolizrAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    MonolizrAudioProcessorEditor (MonolizrAudioProcessor&);
+    ~MonolizrAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    MonolizrAudioProcessor& audioProcessor;
+    juce::Slider midiVolume;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MonolizrAudioProcessorEditor)
+};
